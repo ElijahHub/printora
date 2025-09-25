8b135a7b174d495771d506ff8611902f3ed9db38
Elijah Adewunmi <115023926+ElijahHub@users.noreply.github.com>
2025-09-26 00:53:47 +0100

    client: add shadcn login and sign-up  ui

---
+++ client/src/components/feature.tsx
@@ -0,0 +1,10 @@
+export default function Feature() {
+  return (
+    <section className="">
+      <div className="container mx-auto ">
+        <h2>Featured Products</h2>
+        <div className=""></div>
+      </div>
+    </section>
+  );
+}
